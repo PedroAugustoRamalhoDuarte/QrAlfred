72bbddce1ca799b126fb0f928fe30a62055acf42
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-06-30 15:47:35 -0300

    Revisão de textos da página principal

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import FaceMaskImage from "./images/facemask.jpg"
 import Graphs from "./images/graphs.jpg"
 import CloudImage from "./images/cloud.png"
 import BarImage from "./images/bar.jpg"
+
 function App() {
     return (
         <div>
 
-            <header className="masthead text-center text-white d-flex" style={{backgroundImage: `url(${BarImage})`, opacity: "1.4"}} >
+            <header className="masthead text-center text-white d-flex"
+                    style={{backgroundImage: `url(${BarImage})`, opacity: "1.4"}}>
                 <div className="container my-auto">
                     <div className="row">
                         <div className="col-lg-10 mx-auto">
@@ -24,24 +26,25 @@ function App() {
                     </div>
                     <div className="col-lg-8 mx-auto">
                         <h3 className="text-faded mb-5">Seu restaurante no mundo digital</h3><a
-                        className="btn btn-primary btn-xl js-scroll-trigger" role="button" href="#services">Descubra Mais</a></div>
+                        className="btn btn-primary btn-xl js-scroll-trigger" role="button" href="#services">Descubra
+                        Mais</a></div>
                 </div>
             </header>
 
-            <h1 className="text-success text-center pt-5"> Soluções</h1>
+            <h1 className="text-center pt-5">Nossas Soluções</h1>
             <section>
                 <div className="container">
                     <div className="row align-items-center">
                         <div className="col-lg-6 order-lg-2">
                             <div className="p-5"><img className="rounded-circle img-fluid" src={MenuImage}
-                                                      alt="Imagem de video chaamada"/>
+                                                      alt="Imagem de um cardápio"/>
                             </div>
                         </div>
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5">
                                 <h2 className="display-4">Cárdapio Digital</h2>
-                                <p>Com o QRAlfred seu cardápio está na nuvem e sendo assim de mais fácil edição,
-                                    visualização e manutenção</p>
+                                <p>Com o QRAlfred seu cardápio está na nuvem, sendo assim é mais fácil a edição,
+                                    visualização e manutenção dos itens do cardápio.</p>
                             </div>
                         </div>
                     </div>
@@ -52,7 +55,7 @@ function App() {
                     <div className="row align-items-center">
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5"><img className="rounded-circle img-fluid" src={FoodImage}
-                                                      alt="Prescrição de medicamentos"/>
+                                                      alt="Foto de um prato com salmão"/>
                             </div>
                         </div>
                         <div className="col-lg-6 order-lg-2">
@@ -60,7 +63,8 @@ function App() {
                                 <h2 className="display-4">Controle de Pedidos</h2>
                                 <p>O restaurante tem total controle sobre os pedidos, podendo escolher entre um layout
                                     de lista,
-                                    quadro semelhante ao trello ou layout de como o restaurante é distribuido</p>
+                                    quadro ou layout de como as mesas dos restaurante são distribuidas para uma melhor
+                                    visualização e organizaçao dos pedidos </p>
                             </div>
                         </div>
                     </div>
@@ -71,13 +75,13 @@ function App() {
                     <div className="row align-items-center">
                         <div className="col-lg-6 order-lg-2">
                             <div className="p-5"><img className="rounded-circle img-fluid" src={FormImage}
-                                                      alt="Imagem representando ofertas"/>
+                                                      alt="Imagem representando questionário"/>
                             </div>
                         </div>
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5">
                                 <h2 className="display-4">Avaliação do cliente</h2>
-                                <p>Depois do pagamento o cliente pode avaliar o atendimento/comida com base em um
+                                <p>Depois do pagamento, o cliente pode avaliar o atendimento/comida com base em um
                                     questionário pré-estabelecido pelo restaurante</p>
                             </div>
                         </div>
@@ -89,13 +93,13 @@ function App() {
                     <div className="row align-items-center">
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5"><img className="rounded-circle img-fluid" src={FaceMaskImage}
-                                                      alt="Imagem de um caminhão representando entregas"/>
+                                                      alt="Máscara"/>
                             </div>
                         </div>
                         <div className="col-lg-6 order-lg-2">
                             <div className="p-5">
                                 <h2 className="display-4">Mais Segurança</h2>
-                                <p>Com o cardápio digial a troca de objetos é reduzida entre cliente e funcionários,
+                                <p>Com o cardápio digial, a troca de objetos é reduzida entre cliente e funcionários,
                                     diminuindo a chance de contaminação de doenças</p>
                             </div>
                         </div>
@@ -113,9 +117,11 @@ function App() {
                         <div className="col-lg-6 order-lg-1">
                             <div className="p-5">
                                 <h2 className="display-4">Dados de pedidos</h2>
-                                <p>Um dos grandes benefícios do sistema é ter dados sobre itens mais pedidos, itens mais
-                                    pedidos em conjuntos, horário de pico de cliente que são possíveis ser inferidos
-                                    automaticamente pelo sistema</p>
+                                <p>Um dos grandes benefícios do sistema é conseguir inferir dados sobre o restaurante
+                                    como: itens mais pedidos, itens mais
+                                    pedidos em conjuntos, horário de pico de cliente, dados nos quais são extremamente
+                                    relevantes
+                                    para organiação interna e produtividade do restaurante</p>
                             </div>
                         </div>
                     </div>
