571a2166c884bc7fa9079b2aaddab95f571e8028
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-06-30 15:50:05 -0300

    Updates

---
+++ precache-manifest.8b10a8ee515e6b7094eaf0e6a1858777.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0366733fe277fc14141acdfe972980ac",
+    "url": "/QrAlfred/index.html"
+  },
+  {
+    "revision": "22514055d8b1a538a964",
+    "url": "/QrAlfred/static/css/main.d5be4474.chunk.css"
+  },
+  {
+    "revision": "09d487aab0c0b68e49e6",
+    "url": "/QrAlfred/static/js/2.68715344.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QrAlfred/static/js/2.68715344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22514055d8b1a538a964",
+    "url": "/QrAlfred/static/js/main.c44cb76e.chunk.js"
+  },
+  {
+    "revision": "65ea01860d449bcd1004",
+    "url": "/QrAlfred/static/js/runtime-main.4578e3df.js"
+  },
+  {
+    "revision": "8e1e8d97a2ced1c3e8a1a758c18f74c7",
+    "url": "/QrAlfred/static/media/bar.8e1e8d97.jpg"
+  },
+  {
+    "revision": "d480370fc755f1024e485152fdc13c68",
+    "url": "/QrAlfred/static/media/cloud.d480370f.png"
+  },
+  {
+    "revision": "b6953a7dd32e9132dc3daf6301ae2bbc",
+    "url": "/QrAlfred/static/media/facemask.b6953a7d.jpg"
+  },
+  {
+    "revision": "40d340669dd73504ec6b458176185b6e",
+    "url": "/QrAlfred/static/media/food.40d34066.jpg"
+  },
+  {
+    "revision": "99b1ac308f9d38eb9cfb90583105328b",
+    "url": "/QrAlfred/static/media/form.99b1ac30.png"
+  },
+  {
+    "revision": "7630a61591122d47a990095b7a977ffc",
+    "url": "/QrAlfred/static/media/graphs.7630a615.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QrAlfred/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e732683449849c69aa15dacf5b11debb",
+    "url": "/QrAlfred/static/media/menu.e7326834.png"
+  }
+]);
